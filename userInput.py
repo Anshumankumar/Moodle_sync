c0a546fff918d261235d78e339c57450f9bd006a
Anshuman kumar <anshu266man@gmail.com>
2015-02-09 03:30:30 +0530

    Added the gitignore and userinput
    -Added the feature to get name and password
    -Added the file update feature

---
+++ userInput.py
@@ -0,0 +1,29 @@
+#TODO  Make a gui interface for taking the input
+#TODO  Make the a secure way to save username and password
+
+
+import os.path
+import ast
+
+def userInput():
+    """
+        This function take the user name and password and return
+        If a particular file exist it would seach for file and update info
+        @input: NONE
+        @output: dictiary consisting of user name and password
+    """
+    
+    if os.path.isfile('info'):
+        f = open('info','r')
+        userInfo = ast.literal_eval(f.read())
+    else:    
+        userName = str(raw_input('Enter your Username: '))
+        password = str(raw_input('Enter Your password: '))
+        userInfo = {'username':userName, 'password':password}
+        saveInfoFlag = raw_input("Do you want me to save the username"
+                                 " and password(y/n): ")
+        if saveInfoFlag.lower()[:1] == 'y':
+            f = open('info','w')
+            f.write(str(userInfo))
+    return userInfo
+    
